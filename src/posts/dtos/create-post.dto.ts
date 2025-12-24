33c65d24cbf4712ac847a663879c5e96dcebfc01
Yar Zar Nay Oo <yarzarnayoo27@gmail.com>
2025-12-24 15:06:09 +0630

    docs: add documentation for posts

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -15,17 +15,30 @@ import { postType } from '../enums/postType.enum';
 import { postStatus } from '../enums/postStatus.enum';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'This is the title for the blog post',
+    example: 'Test Title',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: postType,
+    description: "Possible values, 'post', 'page', 'story', 'series'",
+  })
   @IsEnum(postType)
   @IsNotEmpty()
   postType: postType;
 
+  @ApiProperty({
+    description: "For Example - 'my-url'",
+    example: 'my-blog-post',
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)$/, {
@@ -34,34 +47,80 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: postStatus,
+    description: "Possible values, 'draft', 'scheduled', 'review', 'published'",
+  })
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional({
+    description: 'This is the content of the post',
+    example: 'Test Content',
+  })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'Serialize your JSON object else a validation error will be thrown',
+    example:
+      '"{\"type\":\"object\",\"properties\":{\"name\":{\"type\":\"string\"}}}"',
+  })
   @IsJSON()
   @IsOptional()
   schema?: string;
 
+  @ApiPropertyOptional({
+    description: 'Feature image for your blog post',
+    example: '',
+  })
   @IsUrl()
   @IsOptional()
   featuredImageUrl?: string;
 
+  @ApiPropertyOptional({
+    description: 'The date on which the blog post is published',
+    example: '2025-01-15T10:30:00Z',
+  })
   @IsISO8601()
   publishOn: Date;
 
+  @ApiPropertyOptional({
+    description: 'Array of tags passed as string values',
+    example: ['yarzar', 'shweshwe'],
+  })
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   @IsOptional()
   tags: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+          description:
+            'The key can be any string identifier for your meta option',
+          example: 'sidebarEnabled',
+        },
+        value: {
+          type: 'any',
+          description: 'Any value that you want to save to the key',
+          example: true,
+        },
+      },
+    },
+  })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
   @IsOptional()
-  metaOptions: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -15,8 +16,23 @@ export class PostsController {
     return;
   }
 
+  @ApiOperation({
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post is created successfully',
+  })
   @Post()
-  public createPost(@Body() createPostDto: CreatePostDto){
+  public createPost(@Body() createPostDto: CreatePostDto) {}
 
-  }
+  @ApiOperation({
+    summary: 'Updates an existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'A 200 response if the post is updated successfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {}
 }
